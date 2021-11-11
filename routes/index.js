a474b6908b9f3db9e32ddde2d3a208c0ce52c577
Archeel Parekh <archeelparekh11@gmail.com>
2021-11-11 19:06:32 +0530

    Feat: Added index router

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import authRoutes from "./auth.js";
+import userRoutes from "./user.js";
+import teacherRoutes from "./teacher.js";
+import studentRoutes from "./student.js";
+
+import middlewares from "../middlewares/index.js";
+
+import swaggerUi from "swagger-ui-express";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("../openapi.json");
+
+const router = express.Router();
+
+router.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use("/auth", authRoutes);
+
+router.use("/user", middlewares.loginRequired, userRoutes);
+router.use("/teacher", middlewares.loginRequired, middlewares.checkRoleAndId("teacher"), teacherRoutes);
+router.use("/student", middlewares.loginRequired, middlewares.checkRoleAndId("student"), studentRoutes);
+
+export default router;
