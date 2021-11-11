2d7f823624d84f93db5d83d122c2d4c520b92599
Archeel Parekh <archeelparekh11@gmail.com>
2021-11-11 19:05:47 +0530

    Feat: Added Teacher's Lecture Handler

---
+++ handlers/teacher/lecture.js
@@ -0,0 +1,16 @@
+import db from "../../models/index.js";
+
+const createLecture = async (req, res) => {
+  try {
+    const { startTime, endTime, offlineLectureCapacity, onlineLectureLink, vaccinationCriteria } = req.body;
+    const lecture = await db.Lecture.create({ startTime, endTime, offlineLectureCapacity, onlineLectureLink, course:req.params.courseId, vaccinationCriteria });
+    res.status(201).json({ lecture, message: 'Lecture created' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default {
+  createLecture,
+}
