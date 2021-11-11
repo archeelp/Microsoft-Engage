b51128514541115f5f5f841fa7f3d2eb963f2670
Archeel Parekh <archeelparekh11@gmail.com>
2021-11-11 19:03:22 +0530

    Feat:: Added User Handler

---
+++ handlers/user.js
@@ -0,0 +1,16 @@
+import db from '../models/index.js';
+
+const updateProfile = async (req, res) => {
+  try {
+    const { vaccinationStatus } = req.body;
+    await db.User.findOneAndUpdate({_id: req.decodedToken.id}, { vaccinationStatus });
+    res.status(200).json({ message: 'Profile Updated' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default {
+  updateProfile
+};
