cf2c4a5eb86d5b866f6d7af429c6b28c3a52d521
Archeel Parekh <archeelparekh11@gmail.com>
2021-11-15 17:19:07 +0530

    Fix: Text color in enrolled students

--- frontend/src/components/Course/EnrolledStudents.js
+++ frontend/src/components/Course/EnrolledStudents.js
@@ -12,7 +12,7 @@ const EnrolledStudents = ({ enrolledStudents }) => {
             return (
               <div className="py-8 flex flex-wrap md:flex-nowrap" key={index}>
                 <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span className="font-semibold title-font text-gray-700">
+                  <span className="font-semibold title-font text-gray-600">
                     {student.name}
                   </span>
                 </div>
