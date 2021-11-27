7d75b8e3c3eae89fabc84f9c7868019f173d8c38
Archeel Parekh <archeelparekh11@gmail.com>
2021-11-27 19:00:05 +0530

    Fix: API URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:4000";
+export const baseURL = process.env.NODE_ENV === "development"?"http://localhost:4000":"https://microsoft-engage-2021.herokuapp.com";
 export const publicURL = "https://microsoft-engage-2021.ml";
